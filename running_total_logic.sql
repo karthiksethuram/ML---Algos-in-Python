da1091476a10237b3ce8f129bd04e100977ea3c2
Karthikeyan Sethuraman <38745826+karthiksethuram@users.noreply.github.com>
2025-10-29 18:40:26 -0500

    Update running_total_logic.sql

--- running_total_logic.sql
+++ running_total_logic.sql
@@ -35,4 +35,69 @@ WITH RECURSIVE recursive_calc AS (
       CASE
         WHEN b.fill_dt > p.rolling_end THEN 0
         ELSE GREATEST(
-          DATE_DIFF_
+          DATE_DIFF(
+            DATE_ADD(b.fill_dt, INTERVAL CAST(b.day_sply_qty AS INT64) DAY),
+            p.rolling_end,
+            DAY
+          ),
+          0
+        )
+      END AS INT64
+    ) AS excess_supply,
+
+    -- rolling_end always DATE
+    CASE
+      WHEN b.fill_dt > p.rolling_end THEN DATE_ADD(b.fill_dt, INTERVAL CAST(b.day_sply_qty AS INT64) DAY)
+      ELSE (
+        SELECT MAX(d) FROM UNNEST([p.rolling_end, DATE_ADD(b.fill_dt, INTERVAL CAST(b.day_sply_qty AS INT64) DAY)]) AS d
+      )
+    END AS rolling_end,
+
+    -- cumulative_excess
+    CAST(
+      CASE
+        WHEN b.fill_dt > p.rolling_end THEN 0
+        ELSE p.cumulative_excess + GREATEST(
+               DATE_DIFF(DATE_ADD(b.fill_dt, INTERVAL CAST(b.day_sply_qty AS INT64) DAY), p.rolling_end, DAY),
+               0
+             )
+      END AS INT64
+    ) AS cumulative_excess
+
+  FROM recursive_calc p
+  JOIN (
+    SELECT
+      lv1_acct_id,
+      eph_id,
+      drug_cls,
+      ANY_VALUE(drug_name) AS drug_name,
+      fill_year,
+      fill_dt,
+      CAST(MAX(day_sply_qty) AS INT64) AS day_sply_qty,
+      ROW_NUMBER() OVER (
+        PARTITION BY lv1_acct_id, eph_id, drug_cls, fill_year
+        ORDER BY fill_dt
+      ) AS rn
+    FROM `your_dataset.your_table_name`
+    GROUP BY lv1_acct_id, eph_id, drug_cls, fill_year, fill_dt
+  ) AS b
+    ON b.lv1_acct_id = p.lv1_acct_id
+   AND b.eph_id = p.eph_id
+   AND b.drug_cls = p.drug_cls
+   AND b.fill_year = p.fill_year
+   AND b.rn = p.rn + 1
+)
+
+SELECT
+  lv1_acct_id,
+  eph_id,
+  drug_cls,
+  drug_name,
+  fill_year,
+  fill_dt,
+  day_sply_qty,
+  excess_supply,
+  cumulative_excess,
+  rolling_end AS coverage_end_after_fill
+FROM recursive_calc
+ORDER BY lv1_acct_id, eph_id, drug_cls, fill_year, fill_dt;
